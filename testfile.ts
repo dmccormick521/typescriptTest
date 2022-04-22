5978ce701efe6fc7959f31aeb2742e5b5329ffc7
dmccormick521 <dmichaelmccormick@gmail.com>
2022-04-21 22:39:03 -0500

    a bit more messing around

--- testfile.ts
+++ testfile.ts
@@ -1,3 +1,21 @@
 function greet() {
     document.getElementById('click').innerHTML='Stop It';
 }
+
+async function circle(){
+    var c = <HTMLCanvasElement> document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+  
+    for(let x =0; x<5; x++){
+        ctx.arc(65+(x*4),20+(x*4),10,0,2*Math.PI);
+        await new Promise(r => setTimeout(r, 100));
+    }
+
+
+ctx.stroke();
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
